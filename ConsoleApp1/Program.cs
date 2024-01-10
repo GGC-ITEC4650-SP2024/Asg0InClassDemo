b2063b14a7f99b7beff8d2efb0e0b9560de34678
Sebastien Siva <ssiva@ggc.edu>
2024-01-10 10:47:25 -0500

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,9 @@ public class Program
     {
         Console.WriteLine("Hello World!");
         Fruit f = new Fruit("Pear", 1.05);
+
+        float x = 4.5f;
+
         Console.WriteLine(f);
     }
 }
